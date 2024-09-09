60ed3f2f8684f9b2b44de3990f4a8a50784e3dee
JoaHoe/buildenh_jh <joachim.hoehle@outlook.dk>
2024-09-09 12:28:36 +0200

    Update func_det_of_angle.R

--- R/func/func_det_of_angle.R
+++ R/func/func_det_of_angle.R
@@ -5,15 +5,22 @@
 # GNU General Public License (GPL)
 
 det_of_angle <- function(corner_x,corner_y) { 
+  #browser()
   alph <- (atan2(-(corner_y-yc),(corner_x-xc)))*omega # -dy/dx because alpha 
     #in math-coordinate-system
+  cat("dy= ", (corner_y-yc), "\n")
+  cat("dx= ", (corner_x-xc), "\n")
+  
+  #alternative solution (to be tested)
+  #if ((corner_y-yc)<0) {alph=alph+180} 
+  #if ((corner_x-xc)<0) {alph=alph-180}
   
   if(alph < 0) {
-    alph3 <- alph + 360
+    alph3 <- alph + 360 
   } else {
     alph3 <- alph
   } #end if-else
-  
+  cat("alph= ",alph, "alph3= ",alph3,"\n")
   angle_center_corner <- alph3
   return(angle_center_corner)
 } #end of function 'det_of_angle'
